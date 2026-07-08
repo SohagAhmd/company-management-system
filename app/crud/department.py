6ce216c0310f867488cb455609abe7c10c2450bd
Sohag <Sohag.profession@gmail.com>
2026-07-08 13:52:54 +0600

    fix: organize crud and router files before merge

--- app/crud/department.py
+++ app/crud/department.py
@@ -2,59 +2,62 @@ from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.models.department import Department
-from app.schemas.department import Create_model, Get_model, Update_model
+from app.schemas.department import Create_model, Update_model
 
 
 # Add department
 def add_dep(db: Session, payload: Create_model):
-    newDep = Department(name=payload.name)
-    db.add(newDep)
+    new_dep = Department(name=payload.name)
+    db.add(new_dep)
     db.commit()
-    db.refresh(newDep)
-    return newDep
+    db.refresh(new_dep)
+    return new_dep
 
 
-# get all Department
+# Get all Department
 def get_Dep(db: Session):
     all_dep = db.query(Department).all()
     return all_dep
 
 
-# Update Depertment
-def update_dep(id: int, db: Session, playload: Update_model):
-    dep = db.query(Department).filter(Department.id == id).first()
+# Get department by ID
+def get_dep_by_ID(db: Session, dep_id: int):
+    dep = db.query(Department).filter(Department.id == dep_id).first()
 
     if dep is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Department with id {id} not found",
+            detail=f"Department with id {dep_id} not found",
+        )
+    return dep
+
+
+# Update Department
+def update_dep(db: Session, dep_id: int, payload: Update_model):
+    dep = db.query(Department).filter(Department.id == dep_id).first()
+
+    if dep is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Department with id {dep_id} not found",
         )
 
-    update_dep.name = playload.name
-    db.commit(dep)
+    dep.name = payload.name
+    db.commit()
+    db.refresh(dep)
+    return dep
 
 
-# delete dep
-def delete_dep(id: int, db: Session):
-    dep = db.query(Department).filter(Department.id == id).first()
+# Delete department
+def delete_dep(db: Session, dep_id: int):
+    dep = db.query(Department).filter(Department.id == dep_id).first()
 
     if dep is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Department with id {id} not found",
+            detail=f"Department with id {dep_id} not found",
         )
 
     db.delete(dep)
     db.commit()
-
-
-# Get department by ID
-def get_dep_by_ID(payload: int, db: Session):
-    dep = db.query(Department).filter(Department.id == payload).first()
-
-    if dep is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Department with id {payload} not found",
-        )
-    return dep
+    return {"detail": f"Department with id {dep_id} successfully deleted"}
--- app/crud/employee.py
+++ app/crud/employee.py
@@ -1,19 +1,20 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from app.schemas.employee import Create_model, Update_model
+
 from app.models.employee import Employee
+from app.schemas.employee import Create_model, Update_model
 
 
-# Employee create CURD
-def create_emp_in_db(db:Session, emp_data:Create_model):
+# Employee create CRUD
+def create_emp_in_db(db: Session, payload: Create_model):
     new_emp = Employee(
-        first_name = emp_data.first_name,
-        last_name = emp_data.last_name,
-        email = emp_data.email,
-        phone = emp_data.phone,
-        salary = emp_data.salary,
-        job_title = emp_data.job_title,
-        hire_date = emp_data.hire_date
+        first_name=payload.first_name,
+        last_name=payload.last_name,
+        email=payload.email,
+        phone=payload.phone,
+        salary=payload.salary,
+        job_title=payload.job_title,
+        hire_date=payload.hire_date,
     )
     db.add(new_emp)
     db.flush()
@@ -23,41 +24,41 @@ def create_emp_in_db(db:Session, emp_data:Create_model):
 
 
 # Get all employee CRUD
-def get_emp_from_db(db:Session):
+def get_emp_from_db(db: Session):
     employee_list = db.query(Employee).all()
     return employee_list
 
 
-#Get employee by ID
-def get_emp_by_ID_from_db(id:int, db:Session):
-    db_employee = db.query(Employee).filter(Employee.id == id).first()
+# Get employee by ID
+def get_emp_by_ID_from_db(db: Session, emp_id: int):
+    db_employee = db.query(Employee).filter(Employee.id == emp_id).first()
 
     if db_employee is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail=f"Employee with id {id} not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Employee with id {emp_id} not found",
         )
-        
+
     return db_employee
 
 
-#update employee data
-def update_employ(id:int, db:Session, updated_data:Update_model):
-    data_from_db = db.query(Employee).filter(Employee.id == id).first()
+# Update employee data
+def update_employ(db: Session, emp_id: int, payload: Update_model):
+    data_from_db = db.query(Employee).filter(Employee.id == emp_id).first()
 
     if data_from_db is None:
         raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                detail=f"Employee with id {id} not found"
-            )
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Employee with id {emp_id} not found",
+        )
 
-    data_from_db.first_name = updated_data.first_name
-    data_from_db.last_name = updated_data.last_name
-    data_from_db.email = updated_data.email
-    data_from_db.phone = updated_data.phone
-    data_from_db.salary = updated_data.salary
-    data_from_db.job_title = updated_data.job_title
-    data_from_db.hire_date = updated_data.hire_date
+    data_from_db.first_name = payload.first_name
+    data_from_db.last_name = payload.last_name
+    data_from_db.email = payload.email
+    data_from_db.phone = payload.phone
+    data_from_db.salary = payload.salary
+    data_from_db.job_title = payload.job_title
+    data_from_db.hire_date = payload.hire_date
 
     db.add(data_from_db)
     db.commit()
@@ -65,14 +66,16 @@ def update_employ(id:int, db:Session, updated_data:Update_model):
     return data_from_db
 
 
-#Delete employ from db
-def delete_employee(id:int, db:Employee):
-    employee = db.query(Employee).filter(Employee.id == id).first() 
+# Delete employee from db
+def delete_employee(db: Session, emp_id: int):
+    employee = db.query(Employee).filter(Employee.id == emp_id).first()
+
     if employee is None:
         raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                detail=f"Employee with id {id} not found"
-            )
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Employee with id {emp_id} not found",
+        )
 
     db.delete(employee)
     db.commit()
+    return {"detail": f"Employee with id {emp_id} successfully deleted"}
