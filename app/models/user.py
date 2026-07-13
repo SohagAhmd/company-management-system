d5a16da6e1bf2c00b29659047c08057374ef77f6
Sohag <Sohag.profession@gmail.com>
2026-07-13 21:50:17 +0600

    updated model (added role)

--- app/models/user.py
+++ app/models/user.py
@@ -10,5 +10,14 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     hashed_pass = Column(String, nullable=False)
+    role = Column(
+        String,
+        nullable=False,
+        default="user",
+    )
     created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    updated_at = Column(
+        DateTime,
+        default=func.now(),
+        onupdate=func.now(),
+    )
